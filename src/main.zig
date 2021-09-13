2dc8634a3d1d24e24135686360bac0c54ee91cc6
Meghan Denny <hello@nektro.net>
2021-09-12 22:12:04 -0700

    use package name in test program

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-const sqlite = @import("./lib.zig");
+const sqlite = @import("sqlite3");
 
 pub fn main() void {
     std.debug.warn("sqlite version: {s}\n", .{sqlite.c.SQLITE_VERSION});
