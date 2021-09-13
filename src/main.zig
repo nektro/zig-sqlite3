e6a6530cf7d5f3894d6fdf7f9f3867f04fc82b58
Meghan Denny <hello@nektro.net>
2021-09-12 22:11:53 -0700

    strings need {s} now to print

--- src/main.zig
+++ src/main.zig
@@ -2,5 +2,5 @@ const std = @import("std");
 const sqlite = @import("./lib.zig");
 
 pub fn main() void {
-    std.debug.warn("sqlite version: {}\n", .{sqlite.c.SQLITE_VERSION});
+    std.debug.warn("sqlite version: {s}\n", .{sqlite.c.SQLITE_VERSION});
 }
