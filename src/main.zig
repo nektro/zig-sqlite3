700d5ac86239db1c0022e452e9ad0e1f07a10ea3
Meghan Denny <hello@nektro.net>
2022-02-27 18:23:19 -0800

    fix example program

--- src/main.zig
+++ src/main.zig
@@ -2,5 +2,5 @@ const std = @import("std");
 const sqlite = @import("sqlite3");
 
 pub fn main() void {
-    std.debug.warn("sqlite version: {s}\n", .{sqlite.c.SQLITE_VERSION});
+    std.debug.print("sqlite version: {s}\n", .{sqlite.c.SQLITE_VERSION});
 }
